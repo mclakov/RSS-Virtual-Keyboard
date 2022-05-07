2082f9d8c3a560b1fe35c6c8747d9fe9beb0f9f3
Roman Maklakov <mclakov@MacBook-Air-Roman.local>
2022-05-08 02:00:58 +0300

    feat: add some css

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import KEYS from '/js/data.js';
 
-
+console.log(KEYS);
 const container = document.createElement('div');
 const h1 = document.createElement('h1');
 const textBlock = document.createElement('div');
@@ -13,6 +13,7 @@ const row_3 = document.createElement('div');
 const row_4 = document.createElement('div');
 const row_5 = document.createElement('div');
 const nav = document.createElement('div');
+
 container.className = "container";
 textBlock.className = "textBlock";
 textArea.className = "textArea";
@@ -23,6 +24,9 @@ row_3.className = "row3";
 row_4.className = "row4";
 row_5.className = "row5";
 nav.className = "nav";
+
+h1.innerHTML = "Virtual Keyboard"
+h2.innerHTML = "Клавиатура создана в операционной системе Windows Для переключения языка комбинация: левыe ctrl + alt"
 
 
 document.body.append(container);
@@ -40,18 +44,52 @@ container.append(h2);
 
 KEYS.map((elem, index) => {
     let key = document.createElement('div');
-    let keySpan = document.createElement('span');
-
-
+    let keySpanEn = document.createElement('span');
+    let keySpanRu = document.createElement('span');
+    let keySpanEn_fn = document.createElement('span');
+    let keySpanRu_fn = document.createElement('span');
+    // {
+    //     class: "letter key key_b",
+    //     volume: {
+    //     en: "b",
+    //         ru: "и",
+    //         en_fn: "B",
+    //         ru_fn: "И"
+    // }
+    // },
+    keySpanEn.innerHTML = elem.volume.en;
+    keySpanRu.innerHTML = elem.volume.ru;
+    keySpanEn_fn.innerHTML = elem.volume.en_fn;
+    keySpanRu_fn.innerHTML = elem.volume.ru_fn;
+    keySpanRu.className  = "hidden";
+    keySpanEn_fn.className  = "hidden";
+    keySpanRu_fn.className  = "hidden";
 
 
 
 
     key.className = elem.class;
+    key.append(keySpanEn);
+    key.append(keySpanRu);
+    key.append(keySpanEn_fn);
+    key.append(keySpanRu_fn);
+
 
     if (index >=0 && index <=13) {
-
-    }
+        row_1.append(key);
+    };
+    if (index >13 && index <=27) {
+        row_2.append(key);
+    };
+    if (index >27 && index <=40) {
+        row_3.append(key);
+    };
+    if (index >40 && index <=53) {
+        row_4.append(key);
+    };
+    if (index >53 && index <=63) {
+        row_5.append(key);
+    };
         })
 
 
